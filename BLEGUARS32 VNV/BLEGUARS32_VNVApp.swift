ec41f139d8d87728344428853babb28c3563e957
OliverAtondo <atondooliver17@hotmail.com>
2023-12-19 13:06:27 -0800

    Initial Commit

---
+++ BLEGUARS32 VNV/BLEGUARS32_VNVApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEGUARS32_VNVApp.swift
+//  BLEGUARS32 VNV
+//
+//  Created by Daniel Atondo on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BLEGUARS32_VNVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
